2338be4d9fd9799b5173ef364ec8a793b4262047
Bryce <brycepcayanan@gmail.com>
2021-04-08 12:52:26 -0600

    Created rough copy of AddCustomerGUI.java

--- src/view/AddCustomerGUI.java
+++ src/view/AddCustomerGUI.java
@@ -1,7 +1,127 @@
 package view;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class AddCustomerGUI extends JFrame {
-	
+	private JTextField ageField, cardNumberField,
+		nameField, emailField, phoneNumberField, billingAddressField, cardExpiryDateField,
+		genderField, currentVehicleModelField, currentVehicleExteriorField, currentVehicleInteriorField,
+		designatedSalespersonField;
+	private JButton submitNewCustomerButton;
+
+	public AddCustomerGUI(int width, int height) {
+		super("Login");
+		setUndecorated(true);
+		setSize(width, height);
+		setLocationRelativeTo(null);
+		setLayout(new GridLayout(7, 1));
+
+		JLabel ageLabel = new JLabel("Age");
+		ageField = new JTextField();
+		add(ageLabel);
+		add(ageField);
+
+		JLabel nameLabel = new JLabel("Name");
+		nameField = new JTextField();
+		add(nameLabel);
+		add(nameField);
+
+		JLabel currentVehicleModelLabel = new JLabel("Current Vehicle Model");
+		currentVehicleModelField = new JTextField();
+		add(currentVehicleModelLabel);
+		add(currentVehicleModelField);
+
+		JLabel currentVehicleExteriorLabel = new JLabel("Current Vehicle Exterior");
+		currentVehicleExteriorField = new JTextField();
+		add(currentVehicleExteriorLabel);
+		add(currentVehicleExteriorField);
+
+		JLabel currentVehicleInteriorLabel = new JLabel("Current Vehicle Interior");
+		currentVehicleInteriorField = new JTextField();
+		add(currentVehicleInteriorLabel);
+		add(currentVehicleInteriorField);
+
+		JLabel designatedSalespersonLabel = new JLabel("Designated Salesperson");
+		designatedSalespersonField = new JTextField();
+		add(designatedSalespersonLabel);
+		add(designatedSalespersonField);
+
+		JLabel emailLabel = new JLabel("Email");
+		emailField = new JTextField();
+		add(emailLabel);
+		add(emailField);
+		JLabel phoneNumberLabel = new JLabel("Phone Number");
+		phoneNumberField = new JTextField();
+		add(phoneNumberLabel);
+		add(phoneNumberField);
+		JLabel billingAddressLabel = new JLabel("Billing Address");
+		billingAddressField = new JTextField();
+		add(billingAddressLabel);
+		add(billingAddressField);
+		JLabel cardNumberLabel = new JLabel("Card Number");
+		cardNumberField = new JTextField();
+		add(cardNumberLabel);
+		add(cardNumberField);
+		JLabel cardExpiryDateLabel = new JLabel("Card Expiry Date");
+		cardExpiryDateField = new JTextField();
+		add(cardExpiryDateLabel);
+		add(cardExpiryDateField);
+
+		JLabel genderLabel = new JLabel("Gender");
+		genderField = new JTextField();
+		add(genderLabel);
+		add(genderField);
+		submitNewCustomerButton = new JButton("Submit");
+		add(submitNewCustomerButton);
+
+		setVisible(true);
+	}
+
+	public void addSubmitNewCustomerListener(ActionListener actionListener) {
+		submitNewCustomerButton.addActionListener(actionListener);
+	}
+
+	public JTextField getAgeField() {
+		return ageField;
+	}
+	public JTextField getCardNumberField() {
+		return cardNumberField;
+	}
+	public JTextField getNameField() {
+		return nameField;
+	}
+	public JTextField getEmailField() {
+		return emailField;
+	}
+	public JTextField getPhoneField() {
+		return phoneNumberField;
+	}
+	public JTextField getBillingAddressField() {
+		return billingAddressField;
+	}
+	public JTextField getCardExpiryDateField() {
+		return cardExpiryDateField;
+	}
+	public JTextField getGenderField() {
+		return genderField;
+	} 
+	public JTextField getCurrentVehicleModelField() {
+		return currentVehicleModelField;
+	} 
+	public JTextField getCurrentVehicleExteriorField() {
+		return currentVehicleExteriorField;
+	}
+	public JTextField getCurrentVehicleInteriorField() {
+		return currentVehicleInteriorField;
+	}
+	public JTextField getDesignatedSalespersonField() {
+		return designatedSalespersonField;
+	}
+
+	public static void main(String[] args) {
+		new AddCustomerGUI(500, 500);
+	}
+
 }
--- src/view/EditCustomerGUI.java
+++ src/view/EditCustomerGUI.java
@@ -2,17 +2,19 @@ package view;
 
 import javax.swing.JFrame;
 
+import model.Customer;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class EditCustomerGUI extends JFrame {
 
-	private JTextField emailField, phoneField, billingAddressField, cardNumberField, cardExpiryDateField;
+	private JTextField emailField, phoneNumberField, billingAddressField, cardNumberField, cardExpiryDateField, genderField;
 	private JTextArea notesField;
 	private JButton submitNewInformationButton;
 
-	public EditCustomerGUI(int width, int height) {
+	public EditCustomerGUI(int width, int height, Customer customer) {
 		super("Login");
 		setUndecorated(true);
 		setSize(width, height);
@@ -20,29 +22,33 @@ public class EditCustomerGUI extends JFrame {
 		setLayout(new GridLayout(7, 1));
 
 		JLabel emailLabel = new JLabel("Email");
-		emailField = new JTextField();
+		emailField = new JTextField(customer.getEmail());
 		add(emailLabel);
 		add(emailField);
-		JLabel phoneLabel = new JLabel("Phone Number");
-		phoneField = new JTextField();
-		add(phoneLabel);
-		add(phoneField);
+		JLabel phoneNumberLabel = new JLabel("Phone Number");
+		phoneNumberField = new JTextField(customer.getPhoneNumber());
+		add(phoneNumberLabel);
+		add(phoneNumberField);
 		JLabel billingAddressLabel = new JLabel("Billing Address");
-		billingAddressField = new JTextField();
+		billingAddressField = new JTextField(customer.getBillingAddress());
 		add(billingAddressLabel);
 		add(billingAddressField);
 		JLabel cardNumberLabel = new JLabel("Card Number");
-		cardNumberField = new JTextField();
+		cardNumberField = new JTextField(customer.getCardNumber());
 		add(cardNumberLabel);
 		add(cardNumberField);
 		JLabel cardExpiryDateLabel = new JLabel("Card Expiry Date");
-		cardExpiryDateField = new JTextField();
+		cardExpiryDateField = new JTextField(customer.getCardExpiryDate());
 		add(cardExpiryDateLabel);
 		add(cardExpiryDateField);
 		JLabel notesLabel = new JLabel("Notes");
-		notesField = new JTextArea();
+		notesField = new JTextArea(customer.getNotes());
 		add(notesLabel);
 		add(notesField);
+		JLabel genderLabel = new JLabel("Gender");
+		genderField = new JTextField(customer.getGender());
+		add(genderLabel);
+		add(genderField);
 		submitNewInformationButton = new JButton("Submit");
 		add(submitNewInformationButton);
 
@@ -57,7 +63,7 @@ public class EditCustomerGUI extends JFrame {
 		return emailField;
 	}
 	public JTextField getPhoneField() {
-		return phoneField;
+		return phoneNumberField;
 	}
 	public JTextField getBillingAddressField() {
 		return billingAddressField;
