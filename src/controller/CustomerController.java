b800af37164f24d35e01d15e9d8cc54b6411eb17
zachary-lancaster125 <63062366+zachary-lancaster125@users.noreply.github.com>
2021-04-07 20:25:10 -0600

    Added implementation

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import view.*;
+import java.awt.event.*;
+
+import javax.swing.JOptionPane;
 
 public class CustomerController {
 	private MainController main;
@@ -11,7 +14,51 @@ public class CustomerController {
 
 	public CustomerController(MainController main) {
 		this.main = main;
-		new CustomerMenuGUI();
+		customerChoicesMenu = new CustomerMenuGUI();
 	}
-
+	
+	private class CustomerListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == customerChoicesMenu.getEditButton()) {
+				editCustomerMenu = new EditCustomerGUI();
+				main.getNotesArea.setText(main.getCustomer().getNotes());
+			}
+			else if(e.getSource() == editCustomerMenu.getMakeChangesButton()) {
+				if(!(editCustomerMenu.getPhoneNumberField().getText().equals("")) && main.getEmployee().getType().equals("StoreManager")) {
+					main.getCustomer().setPhoneNumber(Integer.parseInt(editCustomerMenu.getPhoneNumberField()).getText());
+				}
+				if(!(editCustomerMenu.getEmailField().getText().equals("")) && main.getEmployee().getType().equals("StoreManager")) {
+					main.getCustomer().setEmail(editCustomerMenu.getEmailField().getText());
+				}
+				if(!(editCustomerMenu.getBillingAddressField().getText().equals("")) && main.getEmployee().getType().equals("StoreManager")) {
+					main.getCustomer().setBillingAddress(editCustomerMenu.getBillingAddressField().getText());
+				}
+				if(!(editCustomerMenu.getCardNumberField().getText().equals("")) && main.getEmployee().getType().equals("StoreManager")) {
+					main.getCustomer().setCardNumber(Integer.parseInt(editCustomerMenu.getCardNumberField().getText()));
+				}
+				if(!(editCustomerMenu.getCardExpiryDateField().getText().equals("")) && main.getEmployee().getType().equals("StoreManager")) {
+					main.getCustomer().setCardExpiryDate(editCustomerMenu.getCardExpiryDateField().getText());
+				}
+				if(!(editCustomerMenu.getNotesArea().getText().equals(""))) {
+					main.getCustomer().setNotes(editCustomerMenu.getNotesArea().getText());
+				}
+				if(!(editCustomerMenu.getVehiclesInterestedIn().getText().equals("")) && main.getEmployee().getType().equals("StoreManager")) {
+					main.getCustomer().addVehiclesInterestedIn(DatabaseManager.searchForVehicle(
+							Integer.parseInt(editCustomerMenu.getVehiclesInterestedIn().getText())));
+				}
+				if(!(editCustomerMenu.getCurrentVehicle().getText().equals("")) && main.getEmployee().getType().equals("StoreManager")) {
+					main.getCustomer().setCurrentVehicle(DatabaseManager.searchForVehicle(
+							(Integer.parseInt(editCustomerMenu.getCurrentVehicle().getText()))));
+				}
+			}
+			else if(e.getSource() == customerChoicesMenu.getViewButton()) {
+				viewCustomerMenu = new ViewCustomerGUI();
+			}
+			else if(e.getSource() == viewCustomerMenu.getSearchButton) {
+				Customer c = DatabaseController.searchForCustomer(viewCustomerMenu.getIDField(), viewCustomerMenu.getNameField());
+				ViewCustomerMenu.display(c);
+			}
+		}
+	}
 }
